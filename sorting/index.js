ee1f875841a0caa1d24b211bd1b25031aa623b34
yaswankar <yaswankar1998@gmail.com>
2024-04-09 22:33:39 +0530

    main: Insertion sort code added

--- sorting/index.js
+++ sorting/index.js
@@ -1,5 +1,6 @@
-const { bubbleSort, selectionSort } = require('./sorting');
+const { bubbleSort, selectionSort, insertionSort } = require('./sorting');
 const list = [1,3,2,5,4,7,6,9,8];
 
-bubbleSort(list);
-selectionSort(list);
+bubbleSort(JSON.parse(JSON.stringify(list)));
+selectionSort(JSON.parse(JSON.stringify(list)));
+insertionSort(JSON.parse(JSON.stringify(list)));
--- sorting/sorting.js
+++ sorting/sorting.js
@@ -10,14 +10,14 @@ function bubbleSort(list) {
             }
         }
     }
-    console.log('Bubble Sort =====> Sorted list ----> O(n2)', list)
+    console.log('Bubble Sort =====> Sorted list ----> O(n2)', list);
 }
 
 function selectionSort(list) {
     const size = list.length;
     let temp;
     for(let i=0; i<size; i++) {
-        const minIdx = i;
+        let minIdx = i;
         // Below part can be recursed
         for(let j=i+1; j<size; j++) {
             if(list[j]<list[minIdx]) {
@@ -28,10 +28,28 @@ function selectionSort(list) {
         list[i] = list[minIdx];
         list[minIdx] = temp;
     }
-    console.log('Selection Sort ====> Sorted list ----> O(n2)', list)
+    console.log('Selection Sort ====> Sorted list ----> O(n2)', list);
+}
+
+function insertionSort(list) {
+    const size = list.length;
+    let temp;
+    for(let pass=1; pass<=size; pass++) {
+        for(let i=0; i<pass; i++) {
+            if(list[pass]<list[i]) {
+                temp = list[pass];
+                for(let j=pass-1; j>=i; j--) {
+                    list[j+1]=list[j];
+                }
+                list[i] = temp;
+            }
+        }
+    }
+    console.log('Insertion Sort ====> Sorted list ----> O(n2)', list)
 }
  
 module.exports = {
     bubbleSort,
     selectionSort,
+    insertionSort,
 }
